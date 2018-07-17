4201c46428d04975c0363db53e5172746b151a80
strabsquad <antunes@geedesign.fr>
2018-07-17 18:43:05 +0200

    first commit

---
+++ wp-content/themes/lauraetarnaud/footer.php
@@ -0,0 +1,33 @@
+
+	</div>
+		<!-- /wrapper -->
+
+<!-- footer -->
+			<footer class="footer" role="contentinfo">
+
+				<!-- copyright -->
+				<p style="padding-top:20px" class="copyright">
+					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. Une production
+					<a href="https://www.brunoantunes.fr" title="Développeur Web freelance Paris" style="text-decoration:underline">Bruno Antunes</a>.
+				</p>
+				<!-- /copyright -->
+
+			</footer>
+			<!-- /footer -->
+
+	
+
+		<?php wp_footer(); ?>
+
+		<!-- analytics -->
+		<script>
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
---
+++ wp-content/themes/lauraetarnaud/page-home.php
@@ -0,0 +1,53 @@
+<?php /* Template Name: home */ get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+			
+            <div class="six columns offset-by-three">
+            <div class="videoWrapper">
+                
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/zerli7Cmh3A?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                
+            </div>
+                <div style="text-align:center">
+                    <a class="cta" href="https://www.annarouch.com/laura-et-arnaud" target="_blank">Accéder aux photos</a>
+                </div>
+                
+            </div>
+            
+            <div class="" style="clear:both">&nbsp;</div>
+            
+            
+
+		
+
+		</section>
+		<!-- /section -->
+        
+        
+        <section id="remerciements">
+        
+            <h2 style="margin-top:20px">REMERCIEMENTS</h2>
+            
+            <div class="eight columns offset-by-two">
+                
+                Voilà un mois tout rond que nous célébrions notre union. Nous voulions vous remercier tous une dernière fois pour votre présence et votre implication dans l'organisation de cette journée, nous garderons à vie cette date du 16 juin 2018. Après un voyage de noces merveilleux, nous voilà de retour, prêts à relever de nouveaux défis.
+                Vous trouverez sur ce site notre vidéo de mariage ainsi que quelques photos prises par Maxime Jeanneton et notre séance photo à Moorea. 
+                
+                <div><a href="/bonus" class="cta rose">Album bonus</a></div>
+            
+            </div>
+            
+            <div class="" style="clear:both">&nbsp;</div>
+        
+        
+        </section>
+        
+        
+	</main>
+
+
+
+<?php get_footer(); ?>
